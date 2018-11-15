83b95a874459bd84bc8c4fb700f45938f9571eed
Kamrul Hasan <ozdreamer.it@gmail.com>
2018-11-15 22:00:23 +1000

    Removed some unnecessary codes.

--- CognitiveDemo/ViewModels/MainViewModel.cs
+++ CognitiveDemo/ViewModels/MainViewModel.cs
@@ -9,13 +9,10 @@ namespace CognitiveDemo
         {
             this.LoginPageCommand = new Command(() => this.Navigate?.Invoke(new SignInPage()));
             this.DescriptionPageCommand = new Command(() => this.Navigate?.Invoke(new DescriptionPage()));
-            this.SpeechToTextPageCommand = new Command(() => this.Navigate?.Invoke(new SpeechToTextPage()));
         }
 
         public Command LoginPageCommand { get; protected set; }
 
         public Command DescriptionPageCommand { get; protected set; }
-
-        public Command SpeechToTextPageCommand { get; protected set; }
     }
 }
